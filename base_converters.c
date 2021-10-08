06e3c661a2f45019d17f56f1472406b2435ece42
cm-amos <amosmwongelah@gmail.com>
2021-10-08 20:51:23 +0300

    Add base_converters.c

--- base_converters.c
+++ base_converters.c
@@ -1,9 +1,4 @@
-/*
- * File: base_converters.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
+#include "main.h"
 
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
